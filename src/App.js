54fea811926be17ac90d582cf4c30c4011004bab
Maliha-Zaman <95135458+Maliha-Zaman@users.noreply.github.com>
2023-11-03 13:13:17 +0600

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//import profilePic from './profile.jpg'; // Import your profile picture
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="container">
+          <nav className="navbar">
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </nav>
+          <div className="profile">
+            <div className="profile-details">
+              <h2>Maliha Zaman</h2>
+              <p>
+                Hello! I'm a passionate web developer with expertise in React.js and front-end technologies.
+                I love creating interactive and responsive web applications. Let's build something amazing together!
+              </p>
+              <div className="buttons">
+                <button>View Profile</button>
+                <a href="/path/to/your/cv.pdf" download><button>Download CV</button></a>
+              </div>
+            </div>
+            {/* <img src={profilePic} alt="Profile" /> */}
+          </div>
+        </div>
       </header>
+      {/* Rest of your content goes here */}
     </div>
   );
-}
+};
 
 export default App;
